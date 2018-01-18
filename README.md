7769c2ee830e1fd4cdd81981d699bcbc3e2f273e
Alexander Panchenko <panchenko.alexander@gmail.com>
2018-01-18 18:59:21 +0100

    adding usage sample, more documentation, now using all neighbors to update label

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Chinese Whispers for Python
 
-This is an implementation of Chinese Whispers in Python.
+This is an implementation of Chinese Whispers in Python based on the NetworkX graph structures. 
 
 [![PyPI version][pypi_badge]][pypi_link] [![Build Status][travis_ci_badge]][travis_ci_link]
 
@@ -8,3 +8,14 @@ This is an implementation of Chinese Whispers in Python.
 [pypi_link]: https://pypi.python.org/pypi/chinese-whispers
 [travis_ci_badge]: https://travis-ci.org/nlpub/chinese-whispers-python.svg
 [travis_ci_link]: https://travis-ci.org/nlpub/chinese-whispers-python
+
+To cluster a graph G represented in a networkx format use the following code:
+
+```python
+from chinese_whispers import chinese_whispers
+chinese_whispers(G, weighting="top", iterations=20)
+```
+
+The clustering results are saved in the "label" attribute of each node.
+
+More usage examples are available in the [sample notebook](samples.ipynb).
--- chinese_whispers/__init__.py
+++ chinese_whispers/__init__.py
@@ -3,37 +3,13 @@ from math import log2
 from random import shuffle
 
 
-def label_weighting(G, node):
-    return max(G.node[neighbor]['label'] for neighbor in G[node])
+def chinese_whispers(G, weighting="top", iterations=20):
+    """ Performs clustering of nodes in a NetworkX graph G
+    using the 'weighting' method. Three weighing schemas are available: 
+    'top' relies on the original weights; 'nolog' normalizes an edge weight 
+    by the degree of the related node; 'log' normalizes an edge weight by the 
+    logariphm of the output degree. """
 
-
-def top_weighting(G, node):
-    return G.node[max(G[node], key=lambda neighbor: G[node][neighbor]['weight'])]['label']
-
-
-def log_weighting(G, node):
-    def weight(neighbor):
-        return G[node][neighbor]['weight'] / log2(G.degree(neighbor) + 1)
-
-    return G.node[max(G[node], key=weight)]['label']
-
-
-def nolog_weighting(G, node):
-    def weight(neighbor):
-        return G[node][neighbor]['weight'] / G.degree(neighbor)
-
-    return G.node[max(G[node], key=weight)]['label']
-
-
-WEIGHTING = {
-    'label': label_weighting,
-    'top': top_weighting,
-    'log': log_weighting,
-    'nolog': nolog_weighting
-}
-
-
-def chinese_whispers(G, weighting, iterations=20):
     for i, node in enumerate(G):
         G.node[node]['label'] = i + 1
 
@@ -45,10 +21,9 @@ def chinese_whispers(G, weighting, iterations=20):
 
         for node in nodes:
             previous = G.node[node]['label']
-
             if G[node]:
-                G.node[node]['label'] = weighting(G, node)
-
+                G.node[node]['label'] = get_new_node_label(G, node, weighting)
+            
             changes = changes or previous != G.node[node]['label']
 
         if not changes:
@@ -57,7 +32,32 @@ def chinese_whispers(G, weighting, iterations=20):
     return G
 
 
+def get_new_node_label(G, node, weighting="top"):
+    """ Updates label of the node based on the local neighborhood of the node.  """
+   
+    labels = defaultdict(float)
+    for neighbor in G[node]:
+        
+        if weighting == "nolog": normalizer = log2(G.degree(neighbor) + 1.0)
+        elif weighting == "log": normalizer = G.degree(neighbor) + 1.0
+        else: normalizer = 1.0
+       
+        if "weight" in G[node][neighbor]:
+             neighbor_impact = G[node][neighbor]["weight"] / normalizer 
+        else:
+            neighbor_impact = 1.0
+
+        labels[G.node[neighbor]["label"]] += neighbor_impact
+
+    top_label = max(labels.items(), key=lambda key_value: key_value[1])[0]
+    
+    return top_label
+
+
 def aggregate_clusters(G):
+    """ Takes as input the labeled graph and outputs a dictionary with the keys
+    being cluster IDs and the values being sets of cluster elements. """
+
     clusters = defaultdict(set)
 
     for node in G:
