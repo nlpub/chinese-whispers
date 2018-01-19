8697812379ff1ba89cce1e9bf5838a026f169613
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2018-01-19 16:56:39 +0100

    Refactor the code

--- chinese_whispers/__init__.py
+++ chinese_whispers/__init__.py
@@ -1,14 +1,41 @@
 from collections import defaultdict
 from math import log2
+from operator import itemgetter
 from random import shuffle
 
+__version__ = '0.3'
 
-def chinese_whispers(G, weighting="top", iterations=20):
+
+def top_weighting(G, node, neighbor):
+    """ A weight is the edge weight. """
+    return G[node][neighbor].get('weight', 1.)
+
+
+def nolog_weighting(G, node, neighbor):
+    """ A weight is the edge weight divided to the node degree. """
+    return G[node][neighbor].get('weight', 1.) / G.degree(neighbor)
+
+
+def log_weighting(G, node, neighbor):
+    """ A weight is the edge weight divided to the log2 of node degree. """
+    return G[node][neighbor].get('weight', 1.) / log2(G.degree(neighbor) + 1)
+
+
+WEIGHTING = {
+    'top': top_weighting,
+    'nolog': nolog_weighting,
+    'log': log_weighting
+}
+
+
+def chinese_whispers(G, weighting='top', iterations=20):
     """ Performs clustering of nodes in a NetworkX graph G
     using the 'weighting' method. Three weighing schemas are available: 
     'top' relies on the original weights; 'nolog' normalizes an edge weight 
     by the degree of the related node; 'log' normalizes an edge weight by the 
-    logariphm of the output degree. """
+    logarithm of the output degree. """
+
+    weighting_func = WEIGHTING[weighting] if isinstance(weighting, str) else weighting
 
     for i, node in enumerate(G):
         G.node[node]['label'] = i + 1
@@ -21,9 +48,10 @@ def chinese_whispers(G, weighting="top", iterations=20):
 
         for node in nodes:
             previous = G.node[node]['label']
+
             if G[node]:
-                G.node[node]['label'] = get_new_node_label(G, node, weighting)
-            
+                G.node[node]['label'] = choose_label(G, node, weighting_func)
+
             changes = changes or previous != G.node[node]['label']
 
         if not changes:
@@ -32,26 +60,17 @@ def chinese_whispers(G, weighting="top", iterations=20):
     return G
 
 
-def get_new_node_label(G, node, weighting="top"):
-    """ Updates label of the node based on the local neighborhood of the node.  """
-   
+def choose_label(G, node, weighting_func):
+    """ Updates the node label based on the local neighborhood of the node. """
+
     labels = defaultdict(float)
+
     for neighbor in G[node]:
-        
-        if weighting == "nolog": normalizer = log2(G.degree(neighbor) + 1.0)
-        elif weighting == "log": normalizer = G.degree(neighbor) + 1.0
-        else: normalizer = 1.0
-       
-        if "weight" in G[node][neighbor]:
-             neighbor_impact = G[node][neighbor]["weight"] / normalizer 
-        else:
-            neighbor_impact = 1.0
+        labels[G.node[neighbor]['label']] += weighting_func(G, node, neighbor)
 
-        labels[G.node[neighbor]["label"]] += neighbor_impact
+    label, _ = max(labels.items(), key=itemgetter(1))
 
-    top_label = max(labels.items(), key=lambda key_value: key_value[1])[0]
-    
-    return top_label
+    return label
 
 
 def aggregate_clusters(G):
