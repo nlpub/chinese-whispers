05331e6b5f4bfc68787f606617684a1bade30dfb
flppgg <18423895+flppgg@users.noreply.github.com>
2019-04-21 15:52:08 +0100

    Now works with weighted graphs and multi-threading

--- chinese-whispers-cython.pyx
+++ chinese-whispers-cython.pyx
@@ -1,23 +1,28 @@
+#cython: boundscheck=False
+
 import numpy as np
 cimport numpy as np
 import networkx as nx
 from datetime import datetime
 import scipy
 cimport cython
-from cython.parallel import prange, threadid
+from cython.parallel import prange
 from libc.stdlib cimport malloc, free
 from collections import Counter
 from matplotlib import pyplot as plt
 
-DTYPE=np.intc
+INT = np.intc
+FLOAT = np.float32
 
-#label_this_node does what it says, basically looking for all the labels of the neighbor nodes and counting them. 
-@cython.boundscheck(False)
-cdef void label_this_node(int[:] arr_all_labels, int[:,:] arr_edges, int[:] arr_cum, int[:] arr_shuff, int c) nogil:
+cdef void label_this_node_unweighted(
+    int arr_all_labels[], 
+    const int arr_edges_o[],
+    const int arr_edges_d[], 
+    const int arr_cum[], 
+    const int arr_shuff[], 
+    int c) nogil:
     
-    cdef int new_label, n, j, count, i, beginning, end, lenght, cum_index
-    
-    #initialize the array of labels for this node's neighbors
+    cdef int node_id, max_i, maxi, new_label, n, j, count, i, beginning, end, lenght, cum_index
     cum_index = arr_shuff[c]
     if cum_index == 0:
         beginning = 0
@@ -25,117 +30,195 @@ cdef void label_this_node(int[:] arr_all_labels, int[:,:] arr_edges, int[:] arr_
         beginning = arr_cum[cum_index - 1]
     end = arr_cum[cum_index]
     lenght = end - beginning
+    node_id = arr_edges_o[beginning] 
 
-    cdef int *arr_to_process = <int *>malloc((lenght) * sizeof(int))
-    cdef int *freq = <int *>malloc((lenght) * sizeof(int))
-    cdef int node_id = arr_edges[beginning, 0] 
+    cdef int *arr_to_process = <int *>malloc(lenght * sizeof(int))
+    cdef int *freq = <int *>malloc(lenght * sizeof(int))
+    if not arr_to_process or not freq:
+        with gil:
+            raise MemoryError()
 
-    for i in range(lenght):
-        n = arr_edges[beginning + i, 1]
-        arr_to_process[i] = arr_all_labels[n]
-    for i in range(lenght):
-        freq[i] = -1
-        
-    #count the labels. This is a simple counter, it will be modified to accomodate for weighted graphs
-    for i in range(lenght):
-        count = 1;
-        for j in range(i + 1, lenght):    
-            if arr_to_process[i] == arr_to_process[j]:      
-                count += 1
-                freq[j] = 0
-        if freq[i] != 0:
-            freq[i] = count
-   
-    #find the highest count and return it
-    cdef int max_i = 0
-    cdef int maxi = freq[0]
-    for i in range(lenght):
-        if freq[i] > maxi:
-            maxi = freq[i]
-            max_i = i 
-    new_label = arr_to_process[max_i]
-    free(arr_to_process)
-    free(freq)
+    try:
+        for i in range(lenght):
+            n = arr_edges_d[beginning + i]
+            arr_to_process[i] = arr_all_labels[n]
 
-    with gil:
-        arr_all_labels[node_id] = new_label
+        for i in range(lenght):
+            freq[i] = -1
 
-#each iteration is a call to this function
-def update_labels(all_labels_v, edges_v, cum_no_neighbors_v):
+        for i in range(lenght):
+            count = 1;
+            for j in range(i + 1, lenght):    
+                if arr_to_process[i] == arr_to_process[j]:      
+                    count += 1
+                    freq[j] = 0
+            if freq[i] != 0:
+                freq[i] = count
+
+        max_i = 0
+        maxi = freq[0]
+        for i in range(lenght):
+            if freq[i] > maxi:
+                maxi = freq[i]
+                max_i = i 
+        new_label = arr_to_process[max_i]
+
+        with gil:
+            arr_all_labels[node_id] = new_label
+
+    finally:
+        free(arr_to_process)
+        free(freq)
+
+cdef void label_this_node_weighted(
+    int arr_all_labels[], 
+    const int arr_edges_o[],
+    const int arr_edges_d[], 
+    const int arr_cum[], 
+    const int arr_shuff[], 
+    const float arr_all_weights[],
+    int c) nogil:
+    
+    cdef int node_id, max_i, new_label, n, j, i, beginning, end, lenght, cum_index
+    cdef float maxi, count
+
+    cum_index = arr_shuff[c]
+    if cum_index == 0:
+        beginning = 0
+    else:
+        beginning = arr_cum[cum_index - 1]
+    end = arr_cum[cum_index]
+    lenght = end - beginning
+    node_id = arr_edges_o[beginning] 
+
+    cdef int *arr_to_process = <int *>malloc(lenght * sizeof(int))
+    cdef float *freq = <float *>malloc(lenght * sizeof(float))
+    cdef float *weights = <float *>malloc(lenght * sizeof(float))
+    if not arr_to_process or not freq or not weights:
+        with gil:
+            raise MemoryError()
+
+    try:
+        for i in range(lenght):
+            n = arr_edges_d[beginning + i]
+            arr_to_process[i] = arr_all_labels[n]
+            weights[i] = arr_all_weights[beginning + i]
+
+        for i in range(lenght):
+            freq[i] = -1
+
+        for i in range(lenght):
+            count = weights[i];
+            for j in range(i + 1, lenght):    
+                if arr_to_process[i] == arr_to_process[j]:      
+                    count += weights[j]
+                    freq[j] = 0
+            if freq[i] != 0:
+                freq[i] = count
+
+        max_i = 0
+        maxi = freq[0]
+        for i in range(lenght):
+            if freq[i] > maxi:
+                maxi = freq[i]
+                max_i = i 
+        new_label = arr_to_process[max_i]
+
+        with gil:
+            arr_all_labels[node_id] = new_label
+
+    finally:
+        free(arr_to_process)
+        free(freq)
+        free(weights)
+
+
+def update_labels(
+    int[::1] all_labels_v, 
+    int[::1] edges_ov, 
+    int[::1] edges_dv, 
+    int[::1] cum_no_neighbors_v,
+    float[::1] weights_v,
+    int threads):
     
     cdef int l = len(cum_no_neighbors_v) 
-    order = np.arange(l, dtype=DTYPE)
+    order = np.arange(l, dtype=INT)
     shuffled = np.copy(order)
     np.random.shuffle(shuffled)
 
-    cdef int [:] shuffled_v = shuffled
+    cdef const int [::1] shuffled_v = shuffled.copy()
     cdef int c
     
-    for c in prange(l, nogil=True, schedule='dynamic', num_threads=1): 
+    if weights_v.size == 1:
+        for c in prange(l, nogil=True, schedule='static', num_threads=threads): 
 
-        label_this_node(all_labels_v, edges_v, cum_no_neighbors_v, shuffled_v, c)
+            label_this_node_unweighted(
+                &all_labels_v[0], 
+                &edges_ov[0], 
+                &edges_dv[0], 
+                &cum_no_neighbors_v[0], 
+                &shuffled_v[0],
+                c)
+    else:
+        for c in prange(l, nogil=True, schedule='static', num_threads=threads): 
 
-#main function. It takes the Networkx graph, puts it in a structure it can understand, and launches the iterations
-def cluster(G, n, draw=False):
+            label_this_node_weighted(
+                &all_labels_v[0], 
+                &edges_ov[0], 
+                &edges_dv[0], 
+                &cum_no_neighbors_v[0], 
+                &shuffled_v[0],
+                &weights_v[0],
+                c)
+
+def corri(G, it=20, weighted=False, threads=1):
 
     start=datetime.now()
 
-    M = nx.to_scipy_sparse_matrix(G,dtype=DTYPE,format='lil')
-    edges = np.asarray(M.nonzero(), dtype=DTYPE).T 
+    M = nx.to_scipy_sparse_matrix(G,dtype=FLOAT,format='lil')
     cdef int nodes = M.shape[0]
 
-    no_neighbors = np.fromiter((scipy.sparse.lil_matrix.count_nonzero(M[i]) for i in range(nodes)), dtype=DTYPE, count=nodes)
-    no_neighbors = no_neighbors[no_neighbors != 0]
+    edges = np.asarray(M.nonzero(), dtype=INT, order='C')
+    edges_o = edges[0] #the origin of the edges
+    edges_d = edges[1] #the destination of the edge
+    cdef const int [::1] edges_ov = edges_o
+    cdef const int [::1] edges_dv = edges_d
 
-    cum_no_neighbors = np.cumsum(no_neighbors, dtype=DTYPE)
+    if weighted:
+        weights = M[(edges_o,edges_d)]
+        weights = weights.toarray(order='C')[0]
+    else:
+        weights = np.asarray([0], dtype=FLOAT)
+    cdef const float [::1] weights_v = weights
+
+    edges=0
     M=0
 
+    cum_no_neighbors = np.where(edges_o[:-1] != edges_o[1:])[0] + 1 
+    cum_no_neighbors = np.append(cum_no_neighbors, len(edges_o))
+    cum_no_neighbors = np.ascontiguousarray(cum_no_neighbors, dtype=INT)
+
     cdef int i
-    all_labels = np.fromiter((i for i in range(1, nodes+1)), dtype=DTYPE, count=nodes)
+    all_labels = np.arange(1, nodes+1, dtype=INT)
+    all_labels = np.ascontiguousarray(all_labels, dtype=INT)
+    
+    cdef int [::1] all_labels_v = all_labels
+    cdef const int [::1] cum_no_neighbors_v = cum_no_neighbors
 
-    cdef int [:] all_labels_v = all_labels
-    cdef int [:,:] edges_v = edges
-    cdef int [:] cum_no_neighbors_v = cum_no_neighbors
-
-    print('finished preparations, updating labels')
+    print('finished preparations, updating labels..')
     print(datetime.now()-start)
 
-    for it in range(n):
-        print('iter %i' %it)
-        update_labels(all_labels_v, edges_v, cum_no_neighbors_v)
+    for iteration in range(it):
+        print('iter %i' %iteration)
+        update_labels(all_labels_v, edges_ov, edges_dv, cum_no_neighbors_v, weights_v, threads)
 
     print(datetime.now()-start)
 
-    most_10 = Counter(all_labels).most_common(10) 
-    print(most_10) #prints the most common 10 labels, with their count
+    most_10 = Counter(all_labels).most_common(10)
+    print(most_10)
 
     for node in G:
         if G[node]:
             G.node[node]['label'] = all_labels[node]
 
-    if draw: draw_graph(G, all_labels)
 
-#A simple way of drawing the graph with the largest clusters in different colors. 
-#Very beta, uses Networkx draw library which is based on Matplotlib. DON'T use on very large graphs as it will crash!
-def draw_graph(G, all_labels):
-    most_5 = Counter(all_labels).most_common(4)
-    red = []
-    green = []
-    yellow = []
-    white = []
-    blue = []
-    for n, l in enumerate(all_labels):
-        if l == most_5[0][0]: blue.append(n)
-        elif l == most_5[1][0]: red.append(n)
-        elif l == most_5[2][0]: green.append(n)
-        elif l == most_5[3][0]: yellow.append(n)
-        else: white.append(n)
-    pos=nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=False, node_size=1, width=1)
-    nx.draw_networkx_nodes(G, pos, nodelist=white, node_color='#cbccc1')
-    nx.draw_networkx_nodes(G, pos, nodelist=red, node_color='r')
-    nx.draw_networkx_nodes(G, pos, nodelist=blue, node_color='b')
-    nx.draw_networkx_nodes(G, pos, nodelist=yellow, node_color='#cfe014')
-    nx.draw_networkx_nodes(G, pos, nodelist=green, node_color='g')
-
-    plt.show()
