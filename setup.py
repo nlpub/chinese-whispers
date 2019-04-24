8e9e0954b15f087e9e5ad315dfeee6282a786d20
flppgg <18423895+flppgg@users.noreply.github.com>
2019-04-24 17:35:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
 from setuptools import setup
 from setuptools.extension import Extension
+from os import path
+
+here = path.abspath(path.dirname(__file__))
 
 try:
     from Cython.Build import cythonize
     from Cython.Distutils import build_ext
     print('Cython is installed, building extension.')
+    USE_CYTHON=True
 except ImportError:
     print('Cython is not installed, using pre-built C file if available.')
 
-ext_modules=[
-    Extension("chinese_whispers.cyt",
-              ["cyt.pyx"],
-              optional=True,
-              extra_compile_args = ["-fopenmp" ],
-              extra_link_args=['-fopenmp'])]
+if USE_CYTHON:
+    ext_modules=[
+         Extension("chinese_whispers.cyt", ["cyt.pyx"], optional=True, extra_compile_args = ["-fopenmp" ], extra_link_args=['-fopenmp'])]
+    cmdclass = {'build_ext': build_ext}
+    opts = {"ext_modules": ext_modules, "cmdclass": cmdclass}
+elif path.isfile(path.join(here, 'cyt.c')):
+    print("Found pre-built C file")
+    ext_modules = [Extension('chinese_whispers.cyt', ['cyt.c'])]
+    cmdclass = {}
+    opts = {"ext_modules": ext_modules, "cmdclass": cmdclass}
+else:
+    print("Pre-built C file is not available. Using pure Python implementation")
+    opts = {}
 
 setup(name='chinese-whispers',
       version='0.5',
@@ -32,6 +43,5 @@ setup(name='chinese-whispers',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',],
       install_requires=['networkx'],
-      cmdclass = {"build_ext": build_ext},
-      ext_modules = ext_modules,
-      zip_safe=True)
+      zip_safe=True,
+      **opts)
