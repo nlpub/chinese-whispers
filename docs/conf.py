7f644a1198f29c28c3a69a778161f49a7cf4b896
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-07-18 23:16:30 +0300

    Ignore tests

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ extensions = [
 
 autoapi_type = 'python'
 autoapi_dirs = ['../chinese_whispers']
+autoapi_ignore = ['test_*.py']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
