7285527793a112c83cac6260f4773888159e5361
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-07-18 23:12:01 +0300

    Use autoapi.extension

--- docs/conf.py
+++ docs/conf.py
@@ -4,19 +4,6 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
-import chinese_whispers
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'Chinese Whispers'
@@ -30,7 +17,7 @@ author = 'Dmitry Ustalov'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'autoapi.extension',
 ]
 
 autoapi_type = 'python'
