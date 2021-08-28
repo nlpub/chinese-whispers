e7290f78fa2cae92b84e968c57569341e83d6445
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2021-08-28 20:33:36 +0300

    Bump version to 0.7.5

--- chinese_whispers/__init__.py
+++ chinese_whispers/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '0.7.4'
+__version__ = '0.7.5'
 __author__ = 'Dmitry Ustalov'
-__credits__ = ['Alexander Panchenko', 'Alexander Chambers']
+__credits__ = ['Alexander Panchenko', 'Alexander Chambers', 'Frederik Wille']
 __license__ = 'MIT'
 
 from .chinese_whispers import WEIGHTING, top_weighting, lin_weighting, log_weighting
