ebabdeb8d5cce909a37782cf1fc41755d080dac6
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2023-04-22 15:03:25 +0200

    Bump version to 0.8.2pre1

--- chinese_whispers/__init__.py
+++ chinese_whispers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.8.2.pre0'
+__version__ = '0.8.2.pre1'
 __author__ = 'Dmitry Ustalov'
 __credits__ = ['Alexander Panchenko', 'Alexander Chambers', 'Frederik Wille']
 __license__ = 'MIT'
