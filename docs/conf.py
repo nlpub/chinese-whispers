ecb89a28ac53cdb425e9ad9eaf1e7abeb0fb6e77
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2023-09-05 16:50:30 +0200

    Use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Chinese Whispers'
-copyright = '2018-2021 <a href="https://github.com/nlpub/chinese-whispers-python">NLPub</a>'
+copyright = '2018-2023 <a href="https://github.com/nlpub/chinese-whispers-python">NLPub</a>'
 author = 'Dmitry Ustalov'
 
 # -- General configuration ---------------------------------------------------
@@ -37,7 +37,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
