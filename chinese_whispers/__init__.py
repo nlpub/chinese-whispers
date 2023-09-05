6c3532960129b69cd81fd2324c1574aad1a9279c
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2023-09-05 21:20:17 +0200

    Bump version to 0.8.2.post2

--- chinese_whispers/__init__.py
+++ chinese_whispers/__init__.py
@@ -4,7 +4,7 @@ Chinese Whispers
 An implementation of the Chinese Whispers clustering algorithm.
 """
 
-__version__ = '0.8.2.post1'
+__version__ = '0.8.2.post2'
 __author__ = 'Dmitry Ustalov'
 __credits__ = ['Alexander Panchenko', 'Alexander Chambers', 'Frederik Wille']
 __copyright = 'Copyright 2018-2023 Dmitry Ustalov'
